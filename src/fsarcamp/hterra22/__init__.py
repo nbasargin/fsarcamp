6295ee6b375fecc5ae1b15445070177487fd18b7
Nikita Basargin <nikita.basargin@dlr.de>
2024-05-03 16:06:16 +0200

    newer experimental moisture loader (currently slow)

--- src/fsarcamp/hterra22/__init__.py
+++ src/fsarcamp/hterra22/__init__.py
@@ -1,6 +1,6 @@
 # Re-exporting internal functionality
 from .fsar import HTERRA22Campaign
-from .moisture import HTERRA22Moisture
+from .moisture import HTERRA22Moisture, HTERRA22MoistureV2
 
 from .constants import APR_28_AM, APR_28_PM, APR_29_AM, APR_29_PM, JUN_15_AM, JUN_15_PM, JUN_16_AM, JUN_16_PM
 from .constants import CREA, CREA_BS_QU, CREA_DW, CREA_SF, CREA_MA
