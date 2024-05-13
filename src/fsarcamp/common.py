442982bf4becd8f8269b94f8e77e2506ef65645f
Nikita Basargin <nikita.basargin@dlr.de>
2024-05-13 09:31:47 +0000

    add looks to pixels conversion

--- src/fsarcamp/common.py
+++ src/fsarcamp/common.py
@@ -68,3 +68,20 @@ def convert_pixels_to_looks(rdp_params, pixels_az, pixels_rg):
     looks_az = max(ml_meters_az / resolution_az, 1)
     looks_rg = max(ml_meters_rg / resolution_rg, 1)
     return looks_az, looks_rg
+
+def convert_looks_to_pixels(rdp_params, looks_az, looks_rg):
+    """
+    Compute the window size in pixels to get the effective number of looks.
+    Parameters:
+        rdp_params is a dictionary (coming from F-SAR metadata XML file) with the following keys:
+            "ps_az", "ps_rg" - Pixel spacing in azimuth and range
+            "res_az", "res_rg" - Processed resolution in azimuth and range
+        looks_az, looks_rg - number of looks in azimuth and range to be obtained
+    Returns
+        pixels_az, pixels_rg - window size in pixels to approximately get the specified number of looks
+    """
+    resolution_az, resolution_rg = float(rdp_params["res_az"]), float(rdp_params["res_rg"]) # Processed resolution
+    # window size in meters to get the specified number of looks
+    meters_az = resolution_az * looks_az
+    meters_rg = resolution_rg * looks_rg
+    return convert_meters_to_pixels(rdp_params, meters_az, meters_rg)
