3b35e1aeb1d961acec5f8f7424629bf39d400631
Nikita Basargin <nikita.basargin@dlr.de>
2024-08-28 14:19:30 +0000

    export dates

--- src/fsarcamp/cropex14/__init__.py
+++ src/fsarcamp/cropex14/__init__.py
@@ -4,9 +4,10 @@ from .moisture import CROPEX14Moisture
 from .biomass import CROPEX14Biomass
 from .field_map import CROPEX14FieldMap
 
-from .dates import MAY_12, MAY_15, MAY_22, MAY_23
-from .dates import JUN_03, JUN_04, JUN_12, JUN_13, JUN_18, JUN_23
-from .dates import JUL_03, JUL_04, JUL_24, JUL_25, AUG_04, SEP_11
+from .dates import APR_09, APR_10, APR_11, MAY_12, MAY_15, MAY_22, MAY_23
+from .dates import JUN_03, JUN_04, JUN_12, JUN_13, JUN_18, JUN_23, JUN_27
+from .dates import JUL_03, JUL_04, JUL_18, JUL_24, JUL_25, JUL_30
+from .dates import AUG_04, AUG_21, AUG_24, SEP_11
 
 from .regions import CORN_C1, CORN_C1_CENTER, CORN_C2, CORN_C1_C2, CORN_C3, CORN_C5, CORN_C6
 from .regions import WHEAT_W1, WHEAT_W2, WHEAT_W4, WHEAT_W5, WHEAT_W7, WHEAT_W10
