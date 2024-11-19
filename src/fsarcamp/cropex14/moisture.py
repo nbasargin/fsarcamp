f5d4f35c3d902cd4c15a0e9324ce9ca44dbe68ec
Nikita Basargin <nikita.basargin@dlr.de>
2024-11-19 16:41:49 +0000

    update cropex moisture loader

--- src/fsarcamp/cropex14/moisture.py
+++ src/fsarcamp/cropex14/moisture.py
@@ -6,27 +6,20 @@ import pathlib
 import datetime
 import numpy as np
 import pandas as pd
+import geopandas as gpd
+import shapely
 import fsarcamp as fc
 import fsarcamp.cropex14 as cr14
 
 class CROPEX14Moisture:
-    def __init__(self, data_folder, cropex14campaign: cr14.CROPEX14Campaign):
+    def __init__(self, data_folder):
         """
         Data loader for soil moisture ground measurements for the CROPEX 2014 campaign.
-
-        Arguments:
-            data_folder: path to the data folder that contains the XLSX files with soil moisture measurements
-            cropex14campaign: reference to the F-SAR campaign, required to geocode points to the SLC coordinates
-
-        Usage example (data paths valid for DLR-HR server as of May 2024):
-            import fsarcamp as fc
-            import fsarcamp.cropex14 as cr14
-            campaign = cr14.CROPEX14Campaign(fc.get_polinsar_folder() / "01_projects/CROPEX/CROPEX14")
-            moisture_folder = fc.get_polinsar_folder() / "Ground_truth/Wallerfing_campaign_May_August_2014/Data/ground_measurements/soil_moisture"
-            moisture = cr14.CROPEX14Moisture(moisture_folder, campaign)
+        `data_folder` defines the data folder that contains the XLSX files with soil moisture measurements.
+        The path on the DLR-HR server as of November 2024:
+        "/data/HR_Data/Pol-InSAR_InfoRetrieval/Ground_truth/Wallerfing_campaign_May_August_2014/Data/ground_measurements/soil_moisture"
         """
         self.data_folder = pathlib.Path(data_folder)
-        self.cropex14campaign = cropex14campaign
 
     def _to_float(self, value):
         try:
@@ -76,21 +69,20 @@ class CROPEX14Moisture:
             "soil_moisture_1", "soil_moisture_2", "soil_moisture_3", "soil_moisture_4", "soil_moisture_5", "soil_moisture_6",
         ])
 
-    def load_soil_moisture_points(self, date_name: str | None = None):
+    def load_soil_moisture_points(self, period_name: str | None = None):
         """
-        Load point soil moisture ground measurements.
-        If a date is provided, only load excel files for that date are loaded.
+        Load soil moisture ground measurements.        
+        If a period is provided, only load excel files for that period are loaded.
         Otherwise, all ground measuremetns from all excel files are loaded.
-        A date is defined by a string (see fsarcamp.cropex14.dates).
-        
-        Returns:
-            Pandas dataframe with following columns:
-                "date_time" - date and time of the measurement, time is missing for some points and set to 0:00
-                "point_id" - point ID, based on the row number of the excel sheet
-                "field" - indicates the field where the point was taken ("Triangular", "Meteo", or "Big")
-                "longitude", "latitude" - geographical coordinates
-                "soil_moisture" - average soil moisture from several samples at that position, value ranges from 0 to 1
-                "soil_moisture_1", ..., "soil_moisture_6" - individual moisture measurements at that position
+        The period is specified by name, see `fsarcamp.cropex14.dates` for allowed values.
+
+        Returns a pandas dataframe with following columns:
+            "date_time" - date and time of the measurement, time is missing for some points and set to 0:00
+            "point_id" - point ID, based on the row number of the excel sheet
+            "field" - indicates the field where the point was taken ("Triangular", "Meteo", or "Big")
+            "longitude", "latitude" - geographical coordinates
+            "soil_moisture" - average soil moisture from several samples at that position, value ranges from 0 to 1
+            "soil_moisture_1", ..., "soil_moisture_6" - individual moisture measurements at that position
         """
         get_path = lambda suffix: self.data_folder / f"Wallerfing_soil_moisture_{suffix}"
         all_dfs = []
@@ -99,47 +91,47 @@ class CROPEX14Moisture:
         name_meteo = "Meteo"
         name_big = "Big" # can further be subdivided into BigCorn, BugCucumbers, BugSugarBeet
         # 2014_04_09
-        if date_name is None or date_name == cr14.APR_09:
+        if period_name is None or period_name == cr14.APR_09:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_04_09.xlsx"), "Triangular Field", 11, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_04_09.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_04_09.xlsx"), "Big Field", 62, name_big))
         # 2014_04_10
-        if date_name is None or date_name == cr14.APR_10:
+        if period_name is None or period_name == cr14.APR_10:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_04_10.xlsx"), "Triangular Field", 11, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_04_10.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_04_10.xlsx"), "Big Field", 47, name_big))
         # 2014_04_11
-        if date_name is None or date_name == cr14.APR_11:
+        if period_name is None or period_name == cr14.APR_11:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_04_11.xlsx"), "Triangular Field", 11, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_04_11.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_04_11.xlsx"), "Big Field", 42, name_big))
         # 2014_05_15
-        if date_name is None or date_name == cr14.MAY_15:
+        if period_name is None or period_name == cr14.MAY_15:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_05_15.xlsx"), "Triangular Field", 9, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_05_15.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_05_15.xlsx"), "Big Field", 51, name_big)) # one point is missing, can be obtained from cucumbers sheet
         # 2014_05_22
-        if date_name is None or date_name == cr14.MAY_22:
+        if period_name is None or period_name == cr14.MAY_22:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_05_22.xlsx"), "Triangular Field", 11, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_05_22.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_05_22.xlsx"), "Big Field", 54, name_big))
         # 2014_06_04
-        if date_name is None or date_name == cr14.JUN_04:
+        if period_name is None or period_name == cr14.JUN_04:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_04.xlsx"), "Triangular Field", 11, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_04.xlsx"), "Meteorological Station", 12, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_04.xlsx"), "Big Field", 46, name_big))
         # 2014_06_12
-        if date_name is None or date_name == cr14.JUN_12:
+        if period_name is None or period_name == cr14.JUN_12:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_12.xlsx"), "Triangular Field", 8, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_12.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_12.xlsx"), "Big Field", 49, name_big))
         # 2014_06_18
-        if date_name is None or date_name == cr14.JUN_18:
+        if period_name is None or period_name == cr14.JUN_18:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_18.xlsx"), "Triangular Field", 11, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_18.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_18.xlsx"), "Big Field", 50, name_big))
         # 2014_06_27 (part 1)
-        if date_name is None or date_name == cr14.JUN_27:
+        if period_name is None or period_name == cr14.JUN_27:
             triangular_2014_06_27 = self._read_soil_moisture_sheet(get_path("2014_06_27.xlsx"), "Triangular Field", 11, name_triangular)
             # point 3 (at index 2) longitude is incorrect, see comments in the spreadsheet
             if triangular_2014_06_27.at[2, "longitude"] == 12.85467:
@@ -151,30 +143,30 @@ class CROPEX14Moisture:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_27.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_06_27.xlsx"), "Big Bare Field", 49, name_big)) # sheet name is different here
         # 2014_07_03
-        if date_name is None or date_name == cr14.JUL_03:
+        if period_name is None or period_name == cr14.JUL_03:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_07_03.xlsx"), "Triangular Field", 12, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_07_03.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_07_03.xlsx"), "Big Bare Field", 46, name_big)) # sheet name is different here
         # 2014_07_18
-        if date_name is None or date_name == cr14.JUL_18:
+        if period_name is None or period_name == cr14.JUL_18:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_07_18.xlsx"), "Triangular Field", 12, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_07_18.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_07_18.xlsx"), "Big Bare Field", 50, name_big)) # sheet name is different here
         # 2014_07_24
-        if date_name is None or date_name == cr14.JUL_24:
+        if period_name is None or period_name == cr14.JUL_24:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_07_24.xlsx"), "Triangular Field", 11, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_07_24.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_07_24.xlsx"), "Big Field", 52, name_big))
         # 2014_07_30
-        if date_name is None or date_name == cr14.JUL_30:
+        if period_name is None or period_name == cr14.JUL_30:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_07_30(Only two Points).xlsx"), "Triangular Field", 2, name_triangular))
         # 2014_08_04
-        if date_name is None or date_name == cr14.AUG_04:
+        if period_name is None or period_name == cr14.AUG_04:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_08_04.xlsx"), "Triangular Field", 11, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_08_04.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_08_04.xlsx"), "Big Field", 38, name_big))
         # 2014_08_21
-        if date_name is None or date_name == cr14.AUG_21:
+        if period_name is None or period_name == cr14.AUG_21:
             # two teams measured triangular field, two sheets available
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_08_21.xlsx"), "Triangular Field (1)", 6, name_triangular, point_id_offset=0))
             # time missing, one measurement incorrect in point 5 for mV, but vol.% is fine
@@ -182,31 +174,42 @@ class CROPEX14Moisture:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_08_21.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_08_21.xlsx"), "Big Field", 52, name_big))
             # 2014_08_24
-        if date_name is None or date_name == cr14.AUG_24:
+        if period_name is None or period_name == cr14.AUG_24:
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_08_24.xlsx"), "Triangular Field", 11, name_triangular))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_08_24.xlsx"), "Meteorological Station", 10, name_meteo))
             all_dfs.append(self._read_soil_moisture_sheet(get_path("2014_08_24.xlsx"), "Big Field", 43, name_big))
         all_points = pd.concat(all_dfs, ignore_index=True)
         return all_points
 
-    def geocode_dataframe(self, df, band, pass_name):
+    def filter_points_by_region(self, points: pd.DataFrame, region_name: str):
         """
-        Extend a dataframe with easting-northing and azimuth-range coordinates (for a specific F-SAR pass).
-        The dataframe must have "latitude" and "longitude" columns defined.
-        Additionals columns are added to the dataframe:
-            "northing", "easting" - geographical coordinates in the LUT coordinate system (UTM zone 33)
-            "lut_northing", "lut_easting" - pixel coordinates within the LUT
-            "azimuth", "range" - pixel coordinates within the SLC
+        Filter the points by the specified region.
+        The region is specified by name, see `fsarcamp.cropex14.regions` for allowed values.
         """
-        fsar_pass = self.cropex14campaign.get_pass(pass_name, band)
+        regions = cr14.CROPEX14Regions()
+        point_locations = gpd.GeoSeries(points.apply(lambda x: shapely.Point(x["longitude"], x["latitude"]), axis=1))
+        geometry = regions.get_geometry_longlat(region_name)
+        result = points[point_locations.within(geometry)]
+        return result
+
+    def geocode_points(self, points: pd.DataFrame, campaign: cr14.CROPEX14Campaign, pass_name: str, band: str):
+        """
+        Geocode the points to the geocoding-lookup-table (LUT) and SLC coordinates (for a specific F-SAR pass).
+        The CROPEX14Campaign must be provided to load the LUT which is required for geocoding.
+        Returns a dataframe with additional columns added:
+            "northing", "easting" - geographical coordinates in the LUT projection (UTM zone 33)
+            "lut_northing", "lut_easting" - pixel indices within the LUT
+            "azimuth", "range" - pixel indices within the SLC
+        """
+        fsar_pass = campaign.get_pass(pass_name, band)
         lut = fsar_pass.load_gtc_sr2geo_lut()
-        latitude = df["latitude"].to_numpy()
-        longitude = df["longitude"].to_numpy()
+        latitude = points["latitude"].to_numpy()
+        longitude = points["longitude"].to_numpy()
         easting, northing = fc.geocode_coords_longlat_to_eastnorth(longitude, latitude, lut.projection)
         lut_northing, lut_easting = fc.geocode_coords_eastnorth_to_lutindices(easting, northing, lut)
         az, rg = fc.geocode_coords_lutindices_to_azrg(lut_northing, lut_easting, lut)
         # extend data frame
-        df_extended = df.assign(
+        df_extended = points.assign(
             northing=northing,
             easting=easting,
             lut_northing=lut_northing,
