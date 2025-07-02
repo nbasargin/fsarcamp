af2e553b33b323af495150d829164d444c44611a
Nikita Basargin <nikita.basargin@dlr.de>
2025-07-02 12:45:46 +0000

    add eitelsried regions

--- src/fsarcamp/cropex25/__init__.py
+++ src/fsarcamp/cropex25/__init__.py
@@ -1,2 +1,4 @@
 # Re-exporting internal functionality
 from .fsar import CROPEX25Campaign
+from .regions import CROPEX25Regions
+from .regions import EITELSRIED_MAIZE, EITELSRIED_POTATO, EITELSRIED_WHEAT
